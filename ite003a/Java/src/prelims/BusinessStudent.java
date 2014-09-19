a7746b5c9f900b4eb9da43f83ed0991e25168de6
Ariel E. Isidro <ariel@asherama.com>
2014-09-19 09:14:29 +0800

    Added prelims exercises

---
+++ ite003a/Java/src/prelims/BusinessStudent.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ite003a.Java.src.prelims;
+
+/**
+ *
+ * @author ariel
+ */
+public class BusinessStudent extends Student{
+    /* Please look at Engineering & Medical Student
+    and try to create your own.
+    */
+    
+}
