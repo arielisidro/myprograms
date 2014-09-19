18d873cbf831d4665baf000df6f39b1853136568
Ariel E. Isidro <ariel@asherama.com>
2014-09-19 09:20:09 +0800

    Added prelims exercises

---
+++ ite003a/Java/src/prelims/StudentProfile.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ite003a.Java.src.prelims;
+
+/**
+ *
+ * @author ariel
+ */
+public class StudentProfile {
+    
+    public static void main(String[] args){
+        
+    }
+    
+}
