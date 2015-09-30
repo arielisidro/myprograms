6b8bf0b25aacd99925817c936db679303c7d0c4c
Ariel E. Isidro <ariel@localhost.localdomain>
2015-09-30 22:50:28 +0800

    with encryption algo

---
+++ python/CaesarCipher.py
@@ -0,0 +1,51 @@
+class CaesarCipher(object):
+
+	characters="abcdefghijklmnopqrstuvwxyz "
+
+	def get_message(self):
+		message=raw_input("Please input message: ")
+		return message
+		
+	def get_shift(self):
+	
+		shift=int(raw_input("Please input number of shifts: "))
+		
+		return shift
+		
+	def encrypt(self,message,shift):
+		encrypted=""
+		l=len(self.characters)
+		lenmessage=len(message)
+		for i in xrange(lenmessage):
+			post=(self.characters.find(message[i])+shift) % l
+			encrypted+=self.characters[post]
+		return encrypted
+		
+	def decrypt(self,message,shift):
+		decrypted=""
+		l=len(self.characters)
+		lenmessage=len(message)
+		for i in xrange(lenmessage):
+			post=(self.characters.find(message[i])-shift) % l
+			decrypted+=self.characters[post]
+		return decrypted
+		
+		
+def main():
+
+	c=CaesarCipher()
+	message=c.get_message()
+	shft=c.get_shift()
+	encrypted=c.encrypt(message,shft)
+	decrypted=c.decrypt(encrypted,shft)
+	
+	print "Message: ",message
+	print "Encrypted: ",encrypted
+	print "Decrypted: ",decrypted
+	
+
+		
+if __name__=="__main__":
+	main()
+	
+	
---
+++ python/count_words.py
@@ -0,0 +1,71 @@
+class Words(object):
+
+	paragraph="The quick brown fox jumps over the lazy dog. "
+	paragraph+="Now is the time for all good men to come to the aide of their country"
+	paragraph+="Peter picked a pack of pickled paper. "
+	paragraph+="The Python programming language was created by" 
+	
+
+	def is_word(self,word,start):
+
+		if start>0:
+			if self.paragraph[start-1]==' ':
+				isword=True
+			else:
+				isword=False
+		else:
+			isword=True
+
+
+		if isword:
+		
+			if len(self.paragraph)>start+len(word):
+				if self.paragraph[start+len(word)]==' ':
+					isword=True
+				else:
+					isword=False
+
+		return isword
+
+	def count_word(self,word):
+
+		return count
+
+	def input_word(self):
+		word=raw_input("Please input word to search: ")
+		return word
+		
+	def search_word(self,word):
+		l=len(self.paragraph)
+		lw=len(word)
+		print ("The word is : %s" % word)
+		for i in xrange(l-lw+1):
+			current_word=self.paragraph[i:i+lw]
+			if current_word.lower() == word.lower():
+				if self.is_word(current_word,i):
+					print "Word found: %s at %d" % (current_word,i)
+		return
+		
+
+	def reverse_word(self):
+		l=len(self.paragraph)-1
+		print "Length : %d " % l
+		while l>=0:
+			print (self.paragraph[l]),
+			l-=1
+		return
+
+	
+	def display_word(self):
+		l=len(self.paragraph)
+		print "Length : %d " % l
+		while l>0:
+			print (self.paragraph[-l]),
+			l-=1
+		return
+
+if __name__=="__main__":
+	x=Words()
+	print x.paragraph
+	x.search_word(x.input_word())
+
